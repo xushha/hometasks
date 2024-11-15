81b7915720c2f250d6e7fbc01dda7fd864849171
xushha <chabanenkoksenia3@gmail.com>
2024-11-15 19:21:00 +0300

    added task 3.2

---
+++ lesson3/task3.1/script.js
@@ -0,0 +1,9 @@
+// primitive data types
+let num =35;
+let str="hello";
+let bint=10n;
+let x;
+n=null;
+let bool=10>15;
+let sym=Symbol();
+console.log('Types we have in JS :\n', typeof(num), typeof(str), typeof(bint),typeof(n),typeof(x),typeof(bool), typeof(sym));
---
+++ lesson3/task3.3/script.js
@@ -0,0 +1,2 @@
+let num='10369';
+console.log(num[0]+' '+num[1]+' '+num[2]+' '+num[3]+' '+num[4]);
