422771881b034ef6bc7be29f1dd16b1ae8384fe8
xushha <chabanenkoksenia3@gmail.com>
2024-11-25 20:17:55 +0300

    task 6.1 done but case sensitive

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,50 +1,4 @@
-// function declaration statement <- подвержен хостингу
-// примитивные копируются значением, а реферальные - ссылками
-// function definition expression <- не подвержен хостингу
 
-// функція вищого порядку - функція може як аргумент приймати функцію та повертити функцію ( є не у всіх мовах програмув)
-// почитать про EcmaScript
-//IIFE
-// function isEquivalent(s,l){
-//     let result ;
-//     if (s === l){
-//         result = '';
-//     } else{
-//         result = s;
-//     }
-//     return result;
-// }
-//
-// console.log(isEquivalent('a', 'a'));
-
-// function deleteLetters(string,letters){
-//     let newLetter = ' ';
-//     for (let i = 0; i < string.length; i++){
-//         for (let j = 0; j < letters.length; j++ ){
-//             if (string.charAt(i) === letters.charAt(j)){
-//                 continue;
-//             } else if (string.charAt(i) !== letters.charAt(j)) {
-//                 newLetter = newLetter + string.charAt(i);
-//             }
-//             console.log(newLetter);
-//         }
-//     }
-//     return newLetter;
-// }
-// function deleteLetters(string,letters){
-//     let newLetter = ' ';
-//     for (let i = 0; i < letters.length; i++){
-//         for (let j = 0; j < string.length; j++ ){
-//             if (string.charAt(j) === letters.charAt(i)){
-//                 continue;
-//             } else {
-//                 newLetter = newLetter + string.charAt(j);
-//             }
-//             console.log(newLetter);
-//         }
-//     }
-//     return newLetter;
-// }
 
 function deleteLetters(string,letters){
     let newLetter = ' ';
@@ -60,7 +14,6 @@ function deleteLetters(string,letters){
                 } else {
                     outString = string.charAt(j);
 
-
                 }
                 if ( relevantLetter !== outString){
                     relevantLetter = outString;
@@ -70,13 +23,18 @@ function deleteLetters(string,letters){
                 }
             newLetter = newLetter + relevantLetter;
 
-        }string = newLetter;
+        }
+        string = newLetter;
         newString = string;
 
 
 
-    }return newString;
+    }
+    return newString;
 }
 
-console.log(deleteLetters('hello world', 'ld'));
+let TestString = prompt('Please input your message');
+let TestLetters = prompt ('please input letters you want to delete from that message');
+
+console.log(deleteLetters(TestString,TestLetters));
 
