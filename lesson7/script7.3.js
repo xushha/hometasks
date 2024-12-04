5f173baad0b7be22c18692fa0175b1edd85c50d3
xushha <chabanenkoksenia3@gmail.com>
2024-12-05 00:14:48 +0200

    task 7.3 done

--- lesson7/script7.3.js
+++ lesson7/script7.3.js
@@ -1,17 +1,18 @@
-function PrintChoice(){
-    for (let i = 0; i < 10; i++){
-        let quantity = prompt(('Please input number under 100:'));
-        if (Number(quantity) < 100 || isNaN(Number(quantity)) || quantity.trim() === ""  ) {
-          alert('Please input right number!')
+function PrintChoice() {
+    let message = 'Your attempts are over. You should have input only numbers and only not under 100.' ;
+    let i = 0;
+    const maxAttempts = 10;
+    do{
+        let number = prompt('Please input number greater than 100');
+        if (isNaN(Number(number) || Number(number) < 100)){
+        } else if(Number(number) > 100) {
+            message = `${number}` ;
+            break;
         }
-        else {
-            return function (){
-
-
-            }
-        }
+        i++;
     }
-
+    while  (i < maxAttempts);
+  return console.log(message);
 }
 
 PrintChoice();
