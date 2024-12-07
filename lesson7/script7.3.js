36ea7a2035a06f89d13451307cd50ec3b5c6db52
xushha <chabanenkoksenia3@gmail.com>
2024-12-07 22:17:19 +0200

    task 7.3 done

--- lesson7/script7.3.js
+++ lesson7/script7.3.js
@@ -1,18 +1,17 @@
+
 function PrintChoice() {
-    let message = 'Your attempts are over. You should have input only numbers and only not under 100.' ;
-    let i = 0;
-    const maxAttempts = 10;
-    do{
-        let number = prompt('Please input number greater than 100');
-        if (isNaN(Number(number) || Number(number) < 100)){
-        } else if(Number(number) > 100) {
-            message = `${number}` ;
+let message ;
+    for (let i = 0; i < 10; i++ ){
+        const input = prompt('Please input number greater than 100');
+        if (Number(input) > 100 ) {
+            message = `${input}`;
             break;
+        } else {
+            message = 'You have run out of attempts. You should have entered  number greater than 100';
         }
-        i++;
     }
-    while  (i < maxAttempts);
-  return console.log(message);
+    return console.log(message);
+
 }
 
 PrintChoice();
