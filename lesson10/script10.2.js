1e27f7af426d7c3949617784c3169de982f44e79
xushha <chabanenkoksenia3@gmail.com>
2024-12-09 01:14:43 +0200

    task 10.2 done

--- lesson10/script10.2.js
+++ lesson10/script10.2.js
@@ -1,9 +1,10 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+const arr2 = [1,'10', true, 3,8, -10];
 function getEvenNumbers(arr) {
-    return arr.filter(i => i % 2 === 0 );
+    return arr.filter(i => typeof(i) === 'number' && i % 2 === 0 );
 }
 
 
 const even = getEvenNumbers(arr);
-console.log(even); // [2, 4, 6, 8]
+console.log(even);
+console.log(getEvenNumbers(arr2));
