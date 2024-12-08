820c00e59a1303b088069d9a0685616a638a6bdb
xushha <chabanenkoksenia3@gmail.com>
2024-12-08 22:45:28 +0200

    task ladder done

---
+++ lesson8/script8.js
@@ -0,0 +1,18 @@
+let ladder = {
+    step : 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        if (this.step !== 0) this.step --;
+        return this;
+    },
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+
+};
+
+ladder.up().up().down().showStep();
