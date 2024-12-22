e934aa5c074872dfd1b3bf15210ea2ed42dab034
xushha <chabanenkoksenia3@gmail.com>
2024-12-22 22:41:53 +0200

    task 11.2 and 11.3 done

--- lesson11/script11.2.js
+++ lesson11/script11.2.js
@@ -1,5 +1,13 @@
-const pic = document.createElement('img');
-const picName = Math.floor(Math.random() * 9) + 1;
-pic.src = `${picName}.jpg`;
-pic.style.width = pic.style.height = '300px';
-document.body.insertAdjacentElement('beforeend',pic);
+const textBlock = document.getElementById('textBlock');
+const button = document.getElementById('colorChanger');
+
+let clicks = 0;
+button.addEventListener('click', () => {
+    clicks++;
+    if (clicks %2!== 0){
+        textBlock.style.color = 'red';
+    }else {
+        textBlock.style.color = '';
+    }
+
+});
