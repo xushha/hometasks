f7bcc7ab1555a7ab1b6951652e1f9d80c480ea0b
xushha <chabanenkoksenia3@gmail.com>
2025-02-01 23:50:00 +0200

    task 16 done

--- lesson16/training.js
+++ lesson16/training.js
@@ -2,24 +2,24 @@ function Student(name, surname, birthDate, assessments = []) {
     this.name = name;
     this.surname = surname;
     this.birthDate = birthDate;
-    this.assessments = assessments;
+    this.assessments = assessments.filter(num => typeof num === 'number' && num >= 0 && num <= 100);
     this.attendance = Array(25).fill(null);
     this.index = 0;
 }
 
 Student.prototype.getAge = function () {
     const nowDate = new Date().getFullYear();
-    return nowDate - this.birthDate;
+    return `Age of ${this.name} : ${nowDate - this.birthDate}`;
 }
 
 Student.prototype.getAverage = function () {
     let sum;
     if (this.assessments.length === 0){
-        return '0';
+        return `${this.name} has no grades :(`;
     } else {
         sum = this.assessments.reduce((total, grade) => total + grade, 0);
     }
-    return sum/this.assessments.length;
+    return `Average rating of ${this.name} : ${sum/this.assessments.length}`;
 
 }
 
@@ -42,21 +42,23 @@ Student.prototype.absent = function () {
 Student.prototype.summary = function () {
 let allClasses = this.attendance.filter(el => el !== null);
 let newAttendance = this.attendance.filter(el => el === true);
-let averageAttendance = newAttendance.length/allClasses.length;
+let averageAttendance = allClasses.length > 0 ? newAttendance.length / allClasses.length : 0;
 let gpaSum = this.assessments.reduce((sum, num) => sum + num, 0);
 let GPA = gpaSum/this.assessments.length;
 if (GPA >= 90 && averageAttendance >= 0.9){
-    return "Молодець!";
-} else if(GPA < 90 && averageAttendance < 0.9){
-    return "Редиска!";
+    return `${this.name} -> Молодець!`;
+} else if(GPA < 90 || averageAttendance < 0.9){
+    return `${this.name} -> Редиска!`;
 } else {
-    return "Добре, але можна краще!";
+    return `${this.name} -> Добре, але можна краще!`;
 }
 }
 
 const student1 = new Student('exampleName1', 'exampleSurname1', 1999, [100, 99, 80, 1]);
 const student2 = new Student('Thomas', 'Shelby',2003,[100,100,90,100,90,90]);
 const student3 = new Student('Tom','Ford',1814, [100,100,100,100,100]);
+const student4 = new Student ('Joe','Goldberg',1980);
+const student5 = new Student('Lana','Rey',1990, ['hi',100,true,80,90]);
 student1.present().absent().present().present();
 student2.present().present().absent().present().present().present().present().present().present().present();
 for(let i = 0; i < 30; i++){
@@ -75,3 +77,7 @@ console.log(student2.summary());
 console.log(student3.getAverage());
 console.log(student3.getAge());
 console.log(student3.summary());
+console.log(student4.getAverage());
+student4.present().present().present();
+console.log(student4.summary())
+console.log(student5.summary());
