d7f9aff00646c883222399bd4f15eb45a9f9c22f
xushha <chabanenkoksenia3@gmail.com>
2025-02-15 17:57:43 +0200

    tasks 17.2 & 17.3 done

--- lesson17/bankAccount.js
+++ lesson17/bankAccount.js
@@ -1,33 +1,39 @@
 class BankAccount {
-   constructor(balance = 0) {
-       this.balance = balance;
-   }
-   getBalance(){
-       return this.balance;
-   }
-   deposit(sum){
-       return this.balance += sum;
-   }
-   withdraw(sum){
-       if(sum > this.balance){
-           console.log("You dont`t have enough money to withdraw");
-           return this.balance;
-       } else{
-           return this.balance -= sum;
-       }
-   }
+    constructor(balance = 0) {
+        this._balance = balance;
+    }
+    getBalance() {
+        return this._balance;
+    }
+    deposit(sum) {
+        if (sum > 0) {
+            return this._balance += sum;
+        } else {
+            console.log("Deposit amount must be positive");
+        }
 
+    }
+    withdraw(sum) {
+        if (sum <= 0) {
+            console.log("Withdraw amount must be positive");
+        }
+        if (sum > this._balance) {
+            console.log("You don't have enough money to withdraw");
+        } else {
+            return this._balance -= sum;
+        }
+    }
 }
 
+console.log("Account 1:")
 const account1 = new BankAccount(1000);
-console.log("account 1:")
-
 console.log(account1.getBalance());
-
 account1.deposit(500);
-
 console.log(account1.getBalance());
-
 account1.withdraw(200);
-
 console.log(account1.getBalance());
+console.log("Account 2:");
+const account2 = new BankAccount();
+account1.withdraw(2000);
+account1.deposit(-100);
+console.log(account2.getBalance());
--- lesson17/coach.js
+++ lesson17/coach.js
@@ -2,7 +2,11 @@ class Coach {
     constructor(name,specialization,rating) {
         this.name = name;
         this.specialization = specialization;
-        this.rating = rating;
+        if (typeof rating !== 'number' || isNaN(rating) || rating < 0 || rating > 5) {
+            this.rating = null;
+        } else {
+            this.rating = rating;
+        }
     }
     displayInfo() {
         console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating:${this.rating}`);
@@ -10,9 +14,9 @@ class Coach {
 }
 
 const coach1 = new Coach('John Doe', 'Fitness', 4.7);
-
 const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+const coach3 = new Coach('Ksu Chabanenko', 'Dance', 'very good');
 
 coach1.displayInfo();
-
 coach2.displayInfo();
+coach3.displayInfo();
